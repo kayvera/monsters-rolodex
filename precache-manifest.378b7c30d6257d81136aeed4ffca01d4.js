a1d06291de84cc33a4a09e9443bd3b7ce1aaf683
kayvera <mikaylarvr@gmail.com>
2020-04-27 21:06:08 -0700

    Updates

---
+++ precache-manifest.378b7c30d6257d81136aeed4ffca01d4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3919d97370ecf756cf302d8be27e1041",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "81070634b3ba5810d500",
+    "url": "/monsters-rolodex/static/css/main.0a6c3132.chunk.css"
+  },
+  {
+    "revision": "a2e4be9481a54b890f97",
+    "url": "/monsters-rolodex/static/js/2.7aaef059.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monsters-rolodex/static/js/2.7aaef059.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "81070634b3ba5810d500",
+    "url": "/monsters-rolodex/static/js/main.f5d4d082.chunk.js"
+  },
+  {
+    "revision": "f051cecc4ee3cb3b301f",
+    "url": "/monsters-rolodex/static/js/runtime-main.eea62ddf.js"
+  }
+]);
